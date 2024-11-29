38ba6795530a3b434cc767bb18806ee9f353b506
Mustafa <ajanovicmustafa14@gmail.com>
2024-11-29 15:30:45 +0100

    more automatization and txt file for API endpoints

--- api_calls.py
+++ api_calls.py
@@ -1,8 +1,98 @@
 import requests
 
-
+def get_api_info(base_url):
+    url = f"http://{base_url}/api/info"
+    headers = {
+        "Content-Type": "application/json",
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()  
+    else:
+        return f"Error: {response.status_code} - {response.text}"  
+    
 
 def set_load_state(base_url, auth_token, load_id, target_state):
+    url = f"http://{base_url}/api/loads/{load_id}/target_state"
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Content-Type": "application/json",
+    }
+    print(headers)
+    response = requests.put(url, headers=headers, data = f"{target_state}")
+    if response.status_code == 200:
+        responseStatus = response.json()['status']
+        if responseStatus   == 'success':
+            return "StateSet"
+        else:
+            return "StateNotSet"
+    else:
+        return 'UnexpectedResponse',print(f"{response.text}")
+    
+def post_account_claim(base_url, user):
+    url = f"http://{base_url}/api/account/claim"
+    headers = {
+        "Content-Type": "application/json",
+    }
+    data = {"user": user}
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        return response.json()  
+    else:
+        return f"Error:{response.text}"  
+    
+def post_network_wlan(base_url, auth_token, ssid, sec, password):
+    url = f"http://{base_url}/api/net/wlans"
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Content-Type": "application/json",
+    }
+    data = {
+        "ssid": ssid,
+        "sec": sec,
+        "password": password
+    }
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        return response.json()  
+    else:
+        return f"Error: {response.text}"  
+    
+
+def post_room_creation(base_url, auth_token, room_name, kind):
+    url = f"http://{base_url}/api/rooms"
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Content-Type": "application/json",
+    }
+    data = {
+        "name": room_name,
+        "kind": kind
+    }
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        return response.json()  
+    else:
+        return f"Error:{response.text}"  
+    
+def patch_load_update(base_url, auth_token, load_id, name, room):
+    url = f"http://{base_url}/api/loads/{load_id}"
+    headers = {
+        "Authorization": f"Bearer {auth_token}",
+        "Content-Type": "application/json",
+    }
+    data = {
+        "name": name,
+        "room": room
+    }
+    response = requests.patch(url, headers=headers, json=data)
+    if response.status_code == 200:
+        return response.json()  
+    else:
+        return f"Error:{response.text}"  
+    
+
+def put_target_state_update(base_url, auth_token, load_id, target_state):
     url = f"http://{base_url}/api/loads/{load_id}/target_state"
     headers = {
         "Authorization": f"Bearer {auth_token}",
--- api_test.robot
+++ api_test.robot
@@ -6,10 +6,18 @@ Library    api_calls.py
 *** Variables ***
 ${BASE_URL}    192.168.0.35
 ${USERNAME}    musa
-${TOKEN}       0599d4dc-ecdf-4798-9bd8-2d5bc2ffe555
+${TOKEN}       1125cf2f-2b28-4929-925f-5b1ab433b5a5
 ${STATESET}    StateSet
-${UPALI}       {\"bri\":5000}
+${UPALI}       {\"bri\":1}
 ${UGASI}       {\"bri\":0}
+${SSID}        203727
+${SEC}         WPA2
+${PASSWORD}    peugeot307
+${ROOM_NAME}   Living room
+${ROOM_KIND}   1 
+${LOAD_NAME}   Living bulb
+${ROOM_ID}     3
+${LOAD_ID}     1
 
 *** Test Cases ***
 Turn On Load
@@ -27,3 +35,26 @@ Turn Off Load
     ${response}=    Set Load State    ${BASE_URL}    ${auth_token}    ${load_id}     ${UGASI}
     Should Be Equal    ${response}    ${STATESET}
 
+Get API Info
+    ${info}=    Get API Info    ${BASE_URL}
+    Log    ${info}
+
+# Post Account Claim
+#     ${response}=    Post Account Claim    ${BASE_URL}    ${USERNAME}
+#     Log    ${response}
+
+# Post Network WLAN
+#     ${response}=    Post Network Wlan    ${BASE_URL}    ${TOKEN}    ${SSID}    ${SEC}    ${PASSWORD}  
+#     Log    ${response}
+
+Post Room Creation
+    ${response}=    Post Room Creation    ${BASE_URL}    ${TOKEN}    ${ROOM_NAME}    ${ROOM_KIND}
+    Log    ${response}
+
+Patch Load Update
+    ${response}=    Patch Load Update    ${BASE_URL}    ${TOKEN}    ${LOAD_ID}    ${LOAD_NAME}    ${ROOM_ID}
+    Log    ${response}
+
+Put Target State Update
+    ${response}=    Put Target State Update    ${BASE_URL}    ${TOKEN}    ${LOAD_ID}     ${UPALI}
+    
